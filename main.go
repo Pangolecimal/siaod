37615c6438bf1cf9b87d02fc3178a573bbe60d5c
Pangolecimal <domkuzaleza@gmail.com>
2023-11-01 01:13:46 +0300

    #2

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 type Node struct {
-	data  int
+	data  string
 	left  *Node
 	right *Node
 }
 
-func (n *Node) insert(data int) error {
-	if n != nil {
+func (n *Node) insert(data string) error {
+	if n == nil {
 		return errors.New("given Node n is nil")
 	}
 	if n.data == data {
@@ -37,9 +38,34 @@ func (n *Node) insert(data int) error {
 }
 
 func (n *Node) print() {
-  if n == nil {}
+	var helper func(n *Node, prefix string, SPC_num int, SPC_char string)
+
+	helper = func(n *Node, prefix string, SPC_num int, SPC_char string) {
+		if n == nil {
+			return
+		}
+		fmt.Print(strings.Repeat(SPC_char, SPC_num*3) + prefix + n.data + "\n")
+		if n.left != nil {
+			helper(n.left, "L: ", SPC_num+1, SPC_char)
+		}
+		if n.right != nil {
+			helper(n.right, "R: ", SPC_num+1, SPC_char)
+		}
+	}
+	helper(n, "&: ", 0, " ")
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	tree := &Node{data: "d"}
+	tree.insert("b")
+	tree.insert("c")
+	tree.insert("a")
+	tree.insert("f")
+	tree.insert("e")
+	tree.insert("g")
+
+	tree.print()
+}
+func print(args ...any) {
+	fmt.Println(args...)
 }
